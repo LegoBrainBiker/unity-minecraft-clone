266cf8b21215f5a7b97dd2c0e27f35a1a3001715
LegoBrainBiker <sameer.brink26@scdsstudent.org>
2024-12-01 12:09:37 -0800

    improved everything

--- Assets/scripts/Chunk.cs
+++ Assets/scripts/Chunk.cs
@@ -1,31 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Chunk
 {
-
-    private static readonly Vector3[] points = {new Vector3(0,0,0), new Vector3(1,0,0), new Vector3(0,1,0), new Vector3(1,1,0), new Vector3(0,0,1), new Vector3(1,0,1), new Vector3(0,1,1), new Vector3(1,1,1)};
+    // all the vertexes of a block
+    private static readonly Vector3Int[] points = {new Vector3Int(0,0,0), new Vector3Int(1,0,0), new Vector3Int(0,1,0), new Vector3Int(1,1,0), new Vector3Int(0,0,1), new Vector3Int(1,0,1), new Vector3Int(0,1,1), new Vector3Int(1,1,1)};
+    // all the vertexes for each side
     private static readonly int[,] points2 = {{2, 6, 7, 3}, {4, 0, 1, 5}, {0, 2, 3, 1}, {5, 7, 6, 4}, {1, 3, 7, 5}, {4, 6, 2, 0}};
-    private static readonly Vector3[] offsets = {new Vector3(0, 1, 0), new Vector3(0, -1, 0), new Vector3(0, 0, -1), new Vector3(0, 0, 1), new Vector3(1, 0, 0), new Vector3(-1, 0, 0)};
+    private static readonly Vector3Int[] offsets = {new Vector3Int(0, 1, 0), new Vector3Int(0, -1, 0), new Vector3Int(0, 0, -1), new Vector3Int(0, 0, 1), new Vector3Int(1, 0, 0), new Vector3Int(-1, 0, 0)};
+    private static readonly Vector3Int[] offsets2 = {new Vector3Int(0, 15, 0), new Vector3Int(0, 0, 0), new Vector3Int(0, 0, 0), new Vector3Int(0, 0, 15), new Vector3Int(15, 0, 0), new Vector3Int(0, 0, 0)};
+    private static readonly Vector3Int[] offsets3 = {new Vector3Int(0, 0, 0), new Vector3Int(0, 15, 0), new Vector3Int(0, 0, 15), new Vector3Int(0, 0, 0), new Vector3Int(0, 0, 0), new Vector3Int(15, 0, 0)};
+    
+    private static readonly Vector3Int[] offsets2x = {new Vector3Int(1, 0, 0), new Vector3Int(1, 0, 0), new Vector3Int(1, 0, 0), new Vector3Int(1, 0, 0), new Vector3Int(0, 1, 0), new Vector3Int(0, 1, 0)};
+    private static readonly Vector3Int[] offsets2y = {new Vector3Int(0, 0, 1), new Vector3Int(0, 0, 1), new Vector3Int(0, 1, 0), new Vector3Int(0, 1, 0), new Vector3Int(0, 0, 1), new Vector3Int(0, 0, 1)};
     private static readonly int[,] textureNumber = {{-1, -1, -1, -1, -1, -1}, {3, 3, 3, 3, 3, 3}, {6, 6, 6, 6, 6, 6}, {7, 7, 7, 7, 7, 7}, {0, 3, 2, 2, 2, 2}, {8, 8, 8, 8, 8, 8}, {4, 4, 5, 5, 5, 5}, {1, 1, 1, 1, 1, 1}};
-    //                                         air, dirt, stone, coblestone, grass, planks, logs leaves
+    //                                         air, dirt, stone, coblestone, grass, planks, logs, leaves
     private GameObject meshObject;
     private Mesh mesh;
+    private GameObject sideMeshObject;
+    private Mesh sideMesh;
+
     int[,,] blocks;
     private Material material;
-    Vector3 chunkPos;
+    Vector3Int chunkPos;
 
-    public Chunk(int[,,] theBlocks, int X, int Y, int Z, Material mat)
+    public Chunk(int[,,] theBlocks, int X, int Y, int Z, Material mat, Chunk[] sideChunks)
     {
-        chunkPos = new Vector3(X*16, Y*16, Z*16);
+        chunkPos = new Vector3Int(X*16, Y*16, Z*16);
         material = mat;
         blocks =  theBlocks;
         generateMesh();
+        generateSideMesh(sideChunks);
     }
 
-    public void generateMesh()
+    private void generateMesh()
     {
+        Object.Destroy(meshObject);
         mesh = new Mesh();
         List<Vector3> verts = new List<Vector3>();
         List<int> triangles = new List<int>();
@@ -64,6 +77,7 @@ public class Chunk
                                 }
                             }
                             else{
+                                /*
                                 for (int i = 0;i<4;i++)
                                 {
                                     pos = points[points2[index, i]]+new Vector3(x, y, z)+chunkPos;
@@ -77,6 +91,7 @@ public class Chunk
                                 triangles.Add(verts.Count-1);
                                 triangles.Add(verts.Count-4);
                                 triangles.Add(verts.Count-2);
+                                */
                             }
                         }
 
@@ -84,9 +99,10 @@ public class Chunk
                 }
             }
         }
-        meshObject = new GameObject();
         if (verts.Count>0)
         {
+            meshObject = new GameObject();
+            meshObject.name = "main mesh of "+chunkPos;
             mesh.SetVertices(verts);
             mesh.SetTriangles(triangles, 0);
             mesh.SetUVs(0, UVs);
@@ -100,29 +116,89 @@ public class Chunk
             meshObject.GetComponents<MeshCollider>()[0].sharedMesh = mesh;
         }
     }
+    public void generateSideMesh(Chunk[] sideChunks) {
+        Object.Destroy(sideMeshObject);
+        sideMesh = new Mesh();
+        List<Vector3> verts = new List<Vector3>();
+        List<int> triangles = new List<int>();
+        Vector3Int pos;
+        Vector3Int pos2;
+        List<Vector2> UVs = new List<Vector2>();
+        int block;
+        int block2;
+        for (int i = 0; i<6; i++) { // block faces
+            if (sideChunks[i] != null) {
+                for (int x = 0; x < 16; x++) { // Position on face
+                    for (int y = 0; y < 16; y++) {
+                        pos = offsets2[i]+offsets2x[i]*x+offsets2y[i]*y;
+                        block = blocks[pos.x,pos.y,pos.z];
+                        pos2 = offsets3[i]+offsets2x[i]*x+offsets2y[i]*y;
+                        block2 = sideChunks[i].blocks[pos2.x,pos2.y,pos2.z];
+                        if (block != 0 && block2 == 0) {
+                                for (int j = 0;j<4;j++) // vertexes
+                                {
+                                    pos2 = points[points2[i, j]]+pos+chunkPos;
+                                    verts.Add(pos2);
+                                    UVs.Add(new Vector2(Mathf.Floor(j/2)+textureNumber[block, i],Mathf.Floor((j+1)%4/2)+15)/16f);
+                                    
+                                }
+                                triangles.Add(verts.Count-4);
+                                triangles.Add(verts.Count-3);
+                                triangles.Add(verts.Count-2);
+                                triangles.Add(verts.Count-1);
+                                triangles.Add(verts.Count-4);
+                                triangles.Add(verts.Count-2);
 
+                        }
+                    }
+                }
+            }
+        }
+        if (verts.Count>0)
+        {
+            sideMeshObject = new GameObject();
+            sideMeshObject.name = "sides mesh of "+chunkPos;
+            sideMesh.SetVertices(verts);
+            sideMesh.SetTriangles(triangles, 0);
+            sideMesh.SetUVs(0, UVs);
+            sideMesh.RecalculateNormals();
+            sideMesh.RecalculateBounds();
+            sideMeshObject.AddComponent(typeof(MeshFilter));
+            sideMeshObject.GetComponents<MeshFilter>()[0].mesh = sideMesh;
+            sideMeshObject.AddComponent(typeof(MeshRenderer));
+            sideMeshObject.GetComponents<MeshRenderer>()[0].material = material;
+            sideMeshObject.AddComponent(typeof(MeshCollider));
+            sideMeshObject.GetComponents<MeshCollider>()[0].sharedMesh = sideMesh;
+        }
+    }
     public int getBlock(int x, int y, int z)
     {
         return blocks[x, y, z];
     }
     
-    public void setBlock(int x, int y, int z, int block)
+    public void setBlock(int x, int y, int z, int block, Chunk[] sideChunks)
     {
         blocks[x, y, z] = block;
-        Object.Destroy(meshObject);
         generateMesh();
+        generateSideMesh(sideChunks);
     }
 
 
     public void dissable()
     {
-        meshObject.SetActive(false);
+        if (meshObject != null)
+            meshObject.SetActive(false);
+        if (sideMeshObject != null)
+            sideMeshObject.SetActive(false);
 
     }
     
     public void enable()
     {
-        meshObject.SetActive(true);
+        if (meshObject != null)
+            meshObject.SetActive(true);
+        if (sideMeshObject != null)
+            sideMeshObject.SetActive(true);
+        
     }
-    
 }
--- Assets/scripts/generateTerrain.cs
+++ Assets/scripts/generateTerrain.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO.Compression;
@@ -6,13 +7,15 @@ using UnityEngine.UIElements;
 
 public class generateTerrain : MonoBehaviour
 {
+    private readonly Vector3Int[] offsets = {new Vector3Int(0, 1, 0), new Vector3Int(0, -1, 0), new Vector3Int(0, 0, -1), new Vector3Int(0, 0, 1), new Vector3Int(1, 0, 0), new Vector3Int(-1, 0, 0)};
     public Transform playerTransform;
-    public GameObject world;
     public Material material;
     public Camera mainCamera;
     public Transform cursor;
     public Dictionary<Vector3,Chunk> chunks = new Dictionary<Vector3,Chunk>();
-    public List<Vector3> activeChunks = new List<Vector3>();
+    public List<Vector3Int> activeChunks = new List<Vector3Int>();
+    // public Vector3[] treeBlockPositions = {new Vector3(0, 0, 0), new Vector3(0, 1, 0), new Vector3(0, 2, 0), new Vector3(0, 3, 0), new Vector3(0, 4, 0), new Vector3(0, 3, 1), new Vector3(0, 3, 2), new Vector3(0, 3, -1), new Vector3(0, 3, -2), new Vector3(1, 3, 0), new Vector3(2, 3, 0), new Vector3(-1, 3, 0), new Vector3(-2, 3, 0)};
+    // public int[] treeBlockTypes = {6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 7};
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +26,7 @@ public class generateTerrain : MonoBehaviour
                 for (int y = -2; y<2; y++)
                 {
                     generateBlocks(x, y, z);
-                    activeChunks.Add(new Vector3(x, y, z));
+                    activeChunks.Add(new Vector3Int(x, y, z));
                 }
             }
         }
@@ -37,7 +40,7 @@ public class generateTerrain : MonoBehaviour
         {
             for (int z = 0; z<16; z++)
             {
-                height = Mathf.PerlinNoise(x/16f+chunkX, z/16f+chunkZ)*5+4;
+                height = Mathf.PerlinNoise(x/16f+chunkX+999999, z/16f+chunkZ+999999)*5+4;
                 for (int y = 0; y<16; y++)
                 {
                     if (y+chunkY*16<height)
@@ -57,10 +60,59 @@ public class generateTerrain : MonoBehaviour
                 }
             }
         }
-        chunks.Add(new Vector3(chunkX, chunkY, chunkZ),new Chunk(blocks, chunkX, chunkY, chunkZ, material));
+
+        chunks.Add(new Vector3Int(chunkX, chunkY, chunkZ),new Chunk(blocks, chunkX, chunkY, chunkZ, material, getNeighborChunks(new Vector3Int(chunkX, chunkY, chunkZ))));
+        updateSideChunks(new Vector3Int(chunkX, chunkY, chunkZ));
+        foreach (Vector3Int offset in offsets) {
+            if (chunks.ContainsKey(new Vector3Int(chunkX, chunkY, chunkZ)+offset))
+                chunks[new Vector3Int(chunkX, chunkY, chunkZ)+offset].generateSideMesh(getNeighborChunks(new Vector3Int(chunkX, chunkY, chunkZ)+offset));
+        }
+        // update side meshes of neighboring chunks
+    }
+
+    private void updateSideChunks(Vector3Int chunkPos) {
+        foreach (Vector3Int offset in offsets) {
+            if (chunks.ContainsKey(chunkPos+offset))
+                chunks[chunkPos+offset].generateSideMesh(getNeighborChunks(chunkPos+offset));
+        }
+    }
+
+    private Chunk[] getNeighborChunks(Vector3Int chunkPos) {
+        
+        Chunk[] sideChunks = new Chunk[6];
+        Vector3Int pos;
+        for (int i = 0; i<6; i++) {
+            pos = chunkPos+offsets[i];
+            if (chunks.ContainsKey(pos))
+                sideChunks[i] = chunks[pos];
+        }
+        return sideChunks;
+    }
+
+    private void setBlock(Vector3Int pos, int block) {
+        Vector3Int chunkPos = new Vector3Int((int)Math.Floor(pos.x/16.0),(int)Math.Floor(pos.y/16.0),(int)Math.Floor(pos.z/16.0));
+        chunks[chunkPos].setBlock(mod(pos.x,16), mod(pos.y,16), mod(pos.z,16), block, getNeighborChunks(new Vector3Int((int)Mathf.Floor(pos.x/16), (int)Mathf.Floor(pos.y/16),(int)Mathf.Floor(pos.z/16))));
+        if (mod(pos.x,16) == 0) {
+            chunks[chunkPos+new Vector3Int(-1, 0, 0)].generateSideMesh(getNeighborChunks(chunkPos+new Vector3Int(-1, 0, 0)));
+        } else if (mod(pos.x,16) == 15) {
+            chunks[chunkPos+new Vector3Int(1, 0, 0)].generateSideMesh(getNeighborChunks(chunkPos+new Vector3Int(1, 0, 0)));
+        }
+        if (mod(pos.y,16) == 0) {
+            chunks[chunkPos+new Vector3Int(0, -1, 0)].generateSideMesh(getNeighborChunks(chunkPos+new Vector3Int(0, -1, 0)));
+        } else if (mod(pos.y,16) == 15) {
+            chunks[chunkPos+new Vector3Int(0, 1, 0)].generateSideMesh(getNeighborChunks(chunkPos+new Vector3Int(0, 1, 0)));
+        }
+        if (mod(pos.z,16) == 0) {
+            chunks[chunkPos+new Vector3Int(0, 0, -1)].generateSideMesh(getNeighborChunks(chunkPos+new Vector3Int(0, 0, -1)));
+        } else if (mod(pos.z,16) == 15) {
+            chunks[chunkPos+new Vector3Int(0, 0, 1)].generateSideMesh(getNeighborChunks(chunkPos+new Vector3Int(0, 0, 1)));
+        }
     }
 
 
+    int mod(int x, int y) {
+        return x-(int)Math.Floor(((double) x)/y)*y;
+    }
 
     // Update is called once per frame
     void Update()
@@ -76,9 +128,8 @@ public class generateTerrain : MonoBehaviour
             {
                 if (hit.distance<4.5f)
                 {
-                    Vector3 pos = hit.point+hit.normal*-0.5f;
-                    
-                    chunks[new Vector3(Mathf.Floor(pos.x/16), Mathf.Floor(pos.y/16), Mathf.Floor(pos.z/16))].setBlock(-(int)Mathf.Floor(pos.x/16)*16+(int)Mathf.Floor(pos.x), -(int)Mathf.Floor(pos.y/16)*16+(int)Mathf.Floor(pos.y), (int)Mathf.Floor(pos.z)-(int)Mathf.Floor(pos.z/16)*16, 0);
+                    Vector3Int pos = Vector3Int.FloorToInt(hit.point+hit.normal*-0.5f);
+                    setBlock(pos, 0);
                     meshHasBeenEditedThisFrame = true;
                 }
             }
@@ -89,6 +140,7 @@ public class generateTerrain : MonoBehaviour
             if (hit.distance<4.5f)
             {
                 Vector3 pos = hit.point+hit.normal*-0.5f;
+                
                 cursor.position = new Vector3(Mathf.Floor(pos.x), Mathf.Floor(pos.y), Mathf.Floor(pos.z));
             }
             else
@@ -101,20 +153,19 @@ public class generateTerrain : MonoBehaviour
         if (Input.GetButtonDown("mouse 1"))
         {
             raycast = mainCamera.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
-
             if (Physics.Raycast(raycast, out hit))
             {
                 if (hit.distance<4.5f)
                 {
-                    Vector3 pos = hit.point+hit.normal*0.5f;
-                    chunks[new Vector3(Mathf.Floor(pos.x/16), Mathf.Floor(pos.y/16), Mathf.Floor(pos.z/16))].setBlock(-(int)Mathf.Floor(pos.x/16)*16+(int)Mathf.Floor(pos.x), -(int)Mathf.Floor(pos.y/16)*16+(int)Mathf.Floor(pos.y), (int)Mathf.Floor(pos.z)-(int)Mathf.Floor(pos.z/16)*16, 3);
+                    Vector3Int pos = Vector3Int.FloorToInt(hit.point+hit.normal*0.5f);
+                    setBlock(pos, 3);
                     meshHasBeenEditedThisFrame = true;
                 }
             }
         }
         if (!meshHasBeenEditedThisFrame)
         {
-            List<Vector3> nearbyChunks = new List<Vector3>();
+            List<Vector3Int> nearbyChunks = new List<Vector3Int>();
 
             for (int x = -4; x<4; x++)
             {
@@ -122,7 +173,7 @@ public class generateTerrain : MonoBehaviour
                 {
                     for (int y = -4; y<4; y++)
                     {
-                        nearbyChunks.Add(new Vector3(Mathf.Floor(playerTransform.position.x/16)+x, Mathf.Floor(playerTransform.position.y/16)+y, Mathf.Floor(playerTransform.position.z/16)+z));
+                        nearbyChunks.Add(new Vector3Int((int)Mathf.Floor(playerTransform.position.x/16)+x, (int)Mathf.Floor(playerTransform.position.y/16)+y, (int)Mathf.Floor(playerTransform.position.z/16)+z));
                     }
                 }
             }
@@ -135,9 +186,9 @@ public class generateTerrain : MonoBehaviour
                     i--;
                 }
             }
-            foreach (Vector3 Pos in nearbyChunks)
+            foreach (Vector3Int Pos in nearbyChunks)
             {
-                if (!activeChunks.Contains(Pos))
+                if (!(meshHasBeenEditedThisFrame ||activeChunks.Contains(Pos)))
                 {
                     if (chunks.ContainsKey(Pos))
                     {
